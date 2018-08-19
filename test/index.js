95628add31a8c97dcde6d9321fc67b04d991ff05
radhey113 <radheyg11@gmail.com>
2018-08-19 23:35:13 +0530

    test config file created for seperating const values for test cases

--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
-process.env.NODE_ENV = 'test';
 
+
+process.env = 'test';
 let mongoose    = require("mongoose");
 var mocha       = require('mocha')
 
 let chai        = require('chai');
 let chaiHttp    = require('chai-http');
 let server      = require('../server.js');
+let testConfig  = require('./testConfig.json');
 let should      = chai.should();
 
 chai.use(chaiHttp);
@@ -16,7 +18,7 @@ describe('Test', () => {
       it('it should GET all the users', (done) => {
         
         /** We can also place server.js file here rather than only link **/ 
-        chai.request('http://localhost:4000')
+        chai.request(testConfig.BaseUrl)
             .get('/getUser')
             .end((err, res) => {
                 
